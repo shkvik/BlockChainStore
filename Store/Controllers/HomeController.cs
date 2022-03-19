5fac3136e818bd145439f8d89004d52e4d899ec6
pendulum <pendulum@DESKTOP-MU1T1VT>
2022-03-19 14:42:17 +0300

    Добавьте файлы проекта.

---
+++ Store/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Models;
+using System.Diagnostics;
+using System.Linq.Expressions;
+
+namespace Store.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+
+        public IActionResult Index()
+        {
+            var some = _configuration["NewValue"];
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
