15bc44495d1a0beb329646e539264364eb0965a7
shkvik <rs.pendulum@gmail.com>
2022-03-19 15:46:41 +0300

    Исправил ссылки, добавил контроллеры

---
+++ Store/Controllers/RegisterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Store.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("IndexRegister");
+        }
+    }
+}
