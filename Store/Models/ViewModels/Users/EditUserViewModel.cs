6c136c807f41c9a6af50ba2af5b4dd9634100d11
shkvik <rs.pendulum@gmail.com>
2022-03-25 17:19:59 +0300

    Добавил управление пользователями

---
+++ Store/Models/ViewModels/Users/EditUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Store.Models.ViewModels.Users
+{
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
